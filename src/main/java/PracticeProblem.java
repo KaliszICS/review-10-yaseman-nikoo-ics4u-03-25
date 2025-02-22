94a5792a1335bc03488356f3d15a471738def797
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-22 23:18:15 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,28 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(pyramid(4));
+	}
+	public static String pyramid(int num){
+		String word="";
+		for (int i=1 ;i <num+1; i++){
+			int x=num-i;
+			for (int z=1; z<num+1;z++){
+				
+				if (z<x){
+					word = word+ " ";}
+				else if(num==1){
+					word="*";
+				}
+				else {
+					word = word+ " *";
+				x--;
+				}
+			}
+			if (i<num-1){
+			word = word +"\n";}
+		}
+		return word;
 	}
 
 }
